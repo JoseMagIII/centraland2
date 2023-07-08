71d83cc6b19e0bd03ec29366066a1cb621bceb90
JoseMagIII <jose_maglaqueiii@dlsu.edu.ph>
2023-07-08 09:18:03 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/pages/_document.tsx
@@ -0,0 +1,23 @@
+import { createGetInitialProps } from '@mantine/next';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+const getInitialProps = createGetInitialProps();
+
+export default class _Document extends Document {
+  static getInitialProps = getInitialProps;
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700&display=swap" rel="stylesheet"/>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head"
+
+
+export default function IndexPage() {
+    return ( 
+    <>
+        <Head>
+            <title>Centraland</title>
+        </Head>
+        <p className="bg-red-600 text-4xl">Hello</p>
+    </> 
+    );
+}
+ 
